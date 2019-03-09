df784bc717688a0ec07798802cb752cddbb04517
atsutko <atsushi.takao.66@gmail.com>
2019-03-09 11:16:28 +0900

    Initial Commit

---
+++ AppleMusicSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppleMusicSample
+//
+//  Created by TakaoAtsushi on 2019/03/09.
+//  Copyright © 2019 TakaoAtsushi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
